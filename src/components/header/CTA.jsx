18b68aba497c817a48b45bfaca2d219855bc155c
ETesauro <emmanuel.tesauro@gmail.com>
2022-03-18 17:44:10 +0100

    Updated Text

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div className="cta">
-        <a href="#" className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'> Let's talk</a>
+    <div className='cta'>
+      <a href='#' className='btn'>
+        Download CV
+      </a>
+      <a href='#contact' className='btn btn-primary'>
+        {' '}
+        Let's talk
+      </a>
     </div>
   )
 }
